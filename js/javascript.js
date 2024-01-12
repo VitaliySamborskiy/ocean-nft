410dfdfddc4b2f81a5f70da65cf5b81645d62542
VitaliySamborskiy <vitaliysamborsky2002@gmail.com>
2024-01-13 01:18:37 +0200

    add scss, add BEM swiper first

--- js/javascript.js
+++ js/javascript.js
@@ -1,7 +1,7 @@
 /* wathc */
 
 document.addEventListener('DOMContentLoaded', function() {
-    const endDate = new Date('2024-01-10T23:59:59').getTime();
+    const endDate = new Date('2024-01-13T23:59:59').getTime();
     const timerInterval = setInterval(updateTimer, 1000);
     function updateTimer () {
         const currentDate = new Date().getTime();
@@ -9,6 +9,7 @@ document.addEventListener('DOMContentLoaded', function() {
         if (timeLeft <= 0) {
             clearInterval(timerInterval);
             document.getElementById('countdown').innerHTML = '00 00 00';
+            document.getElementById('countdown-1').innerHTML = '00 00 00';
         } else {
             const hours = Math.floor((timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
             const minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
@@ -16,7 +17,9 @@ document.addEventListener('DOMContentLoaded', function() {
             document.getElementById('hours').innerHTML = formatTime(hours);
             document.getElementById('minutes').innerHTML = formatTime(minutes);
             document.getElementById('seconds').innerHTML = formatTime(seconds);
-            console.log(hours, minutes, seconds);
+            document.getElementById('slide-hours').innerHTML = formatTime(hours);
+            document.getElementById('slide-minutes').innerHTML = formatTime(minutes);
+            document.getElementById('slide-seconds').innerHTML = formatTime(seconds);
         }
     }
     function formatTime(time) {
@@ -26,23 +29,23 @@ document.addEventListener('DOMContentLoaded', function() {
 
 /*burger-menu*/
 
-document.addEventListener('DOMContentLoaded', function () {
     const burgerMenu = document.querySelector('.burger-menu');
     burgerMenu.addEventListener ('click', function () {
         this.classList.toggle('active');
-        console.log('click')
-    }) 
-})
+    })
 
 /*main-swiper*/
 /*
 const swiper = new Swiper('.swiper', {
     // Optional parameters
     loop: true,
-    slidesPerView: 1, 
-    spaceBetween: 0,
+    slidesPerView: 2, 
+    spaceBetween: 100,
             autoplay: {
                 delay: 10000, 
                 disableOnInteraction: false,
-            }, 
+            },
+            navigation: {
+                nextEl: '.swiper-button-next',
+              }, 
 });*/
